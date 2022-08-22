98afd2d0f36df9051bc6e80e035aaefc0c14acb1
ddidee <olawaleadeola20@gmail.com>
2022-08-22 15:03:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const path = require('path')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+
+
+dotenv.config({path: 'config.env'})
+let PORT = process.env.PORT||8000
+
+
+app.use(morgan('tiny'))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.set('view engine', 'ejs')
+
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
+app.use('/register.html', express.static(path.resolve(__dirname, 'assets/css/register.css')))
+
+
+
+
+app.get('/', (req, res) => {
+    // res.send('SAIL DEMON APP')
+    res.render('index')
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`this is currently available in port ${PORT}`);
+})
